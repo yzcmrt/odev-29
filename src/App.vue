67d4e376f8cf75b8b9cade7a64c72cf7df650109
Mert Yazici <yazici.mert.35@gmail.com>
2023-06-13 23:20:17 +0300

    ödev-29

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+
+<template>
+  <div id="app">
+    <calculator></calculator>
+  </div>
+  </template>
+  
+<script>
+import Calculator from './components/Calculator.vue'
+
+export default {
+  name:'app',
+  components: {
+    Calculator
+  }
+
+}
+</script>
